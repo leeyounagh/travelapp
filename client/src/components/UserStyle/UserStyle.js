635b088e480c090060176482f39fc184d7d7cb5d
youngah lee <dudgk0216@naver.com>
2022-06-11 02:35:26 +0900

    좋아요 페이지 렌더링

--- client/src/components/UserStyle/UserStyle.js
+++ client/src/components/UserStyle/UserStyle.js
@@ -1,28 +1,41 @@
 import React,{ useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import {getGoodItems} from '../../_actions/User_action'
+import './UserStyle.scss'
 
 
 const UserStyle = (props) => {
-    //  let goodItems = [{id:[],image:[],address:[],title:[]}]
-    //  let dispatch = useDispatch()
-    //  console.log(goodItems)
-    // useEffect(()=>{
-     
-    //     if(props.user.userData && props.user.userData.good){
-    //         if(props.user.userData.good.length>0){
-    //            props.user.userData.good.forEach(item => {
-    //                goodItems.push({id:item.id,image:item.image,
-    //             address:item.address,title:item.title})
-    //            });
-    //            dispatch(getGoodItems(goodItems,props.user.userData.good))
-    //         }
-    //     }
-    // },[])
 
+//    console.log(props.user.userData.good)
+
+useEffect(()=>{
+    test()
+},[])
+
+const test = ()=>{
+    if(props.user.userData&&props.user.userData.good){
+        console.log(props.user.userData.good,props.user.userData.good[0].image)
+      
+        
+        
+        return props.user.userData.good.map((item)=>{
+            return (<div style={{marginTop:'20px'
+            }}>
+                <h2>{item.title}</h2>
+                 <img alt={item.title} src={item.image} width='300px' height ='300px'></img>
+            </div>)
+          })
+    }
+}
+ 
+// console.log(props.user.userData.good)
     return (
-        <div>
-           UserStyle 
+        <div style={{position:'absolute', marginTop:'20px',top:'200px',left:'200px',
+        }}>
+            
+
+        {test()}
+          
         </div>
     );
 };
